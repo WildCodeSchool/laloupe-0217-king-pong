c79db3fd428e72291c4db893d2ca810520d63ee8
Djodjonx <djo.moutier@gmail.com>
2017-04-25 15:40:33 +0200

    creation list of community in community.html, adding a scope for Ng-repeat and function for Ng-click

--- app/js/controllers/community.js
+++ app/js/controllers/community.js
@@ -1,5 +1,16 @@
 angular.module('app')
-    .controller('CommunityController', function($scope, $timeout, $mdSidenav) {
-
-
-    });
+  .controller('CommunityController', function($scope, $timeout, $mdSidenav,UserService, CommunityService) {
+    $scope.communitys = [{
+      _id: 1,
+      name: "un"
+    }, {
+      _id: 2,
+      name: "deux"
+    }, {
+      _id: 3,
+      name: "trois"
+    }];
+    $scope.addCommunity = function(id){
+      console.log(id);
+    };
+  });
