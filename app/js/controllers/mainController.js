00add0661438d724e99bd868d6a226b7edc1025b
Djodjonx <djo.moutier@gmail.com>
2017-04-26 17:00:30 +0200

    final version of login and register with all services and database

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,12 +1,12 @@
 angular.module('app')
-    .controller('MainController', function($scope, $timeout, $mdSidenav) {
+  .controller('MainController', function($scope, $timeout, $mdSidenav) {
 
 
 
-  $('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-  shift: -150,
-padding: 80,
-});
-
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      shift: -150,
+      padding: 80,
     });
+
+  });
--- generated/angular/template-cache.js
+++ generated/angular/template-cache.js
@@ -206,31 +206,46 @@ angular.module("app").run(["$templateCache", function($templateCache) {
   );
 
   $templateCache.put("anon/login.html",
-    "\n" +
     "<div class=\"row\">\n" +
-    "  <form class=\"col s12 l6 offset-l4 login\" name=\"loginForm\" novalidate ng-submit=\"login()\">\n" +
+    "  <div class=\"welcomeLogin background z-depth-3\">\n" +
+    "    <h1 class=\"titre center-align \">KingPong</h1>\n" +
+    "\n" +
+    "  </div>\n" +
+    "</div>\n" +
+    "<div class=\"row\">\n" +
+    "  <form class=\"col s12 offset-l4 l4 login\" name=\"loginForm\" novalidate ng-submit=\"login()\">\n" +
     "    <div ng-repeat=\"error in errors\">{{error.error}}</div>\n" +
     "    <div class=\"row\">\n" +
-    "      <div class=\"input-field col s11 l6 \">\n" +
+    "      <div class=\"input-field col s11 offset-l1 l10 \">\n" +
     "        <i class=\"material-icons prefix\">mail</i>\n" +
     "        <input id=\"email\" type=\"email\" class=\"form-control\" ng-model=\"user.email\" required>\n" +
     "        <label for=\"email\">Email</label>\n" +
     "      </div>\n" +
     "    </div>\n" +
     "    <div class=\"row\">\n" +
-    "      <div class=\"input-field col s11 l6 \">\n" +
+    "      <div class=\"input-field col s11 offset-l1 l10 \">\n" +
     "        <i class=\"material-icons prefix\">vpn_key</i>\n" +
     "        <input id=\"password\" type=\"password\" class=\"form-control\" ng-model=\"user.password\" required>\n" +
     "        <label for=\"password\">Password</label>\n" +
     "      </div>\n" +
     "    </div>\n" +
     "    <div class=\"row\">\n" +
-    "      <div class=\"col s12 l6\">\n" +
-    "        <a id=\"submit\" ng-click=\"login()\" type=\"submit\" class=\"waves-effect waves-teal btn \">Register</a>\n" +
+    "      <div class=\"col col s12 offset-l1 l10\">\n" +
+    "        <a id=\"submit\" ng-click=\"login()\" type=\"submit\" class=\"waves-effect waves-teal btn \">Login</a>\n" +
     "      </div>\n" +
     "    </div>\n" +
     "  </form>\n" +
-    "</div>\n"
+    "</div>\n" +
+    "<footer>\n" +
+    "  <div class=\"row\">\n" +
+    "    <div class=\"col s12 offset-l4 l4 center-align\">\n" +
+    "      <ul>\n" +
+    "        <li>Vous n’avez pas de compte?</li>\n" +
+    "        <li><a class=\"center-align\" ui-sref='anon.register'>Inscrivez-vous</a></li>\n" +
+    "      </ul>\n" +
+    "    </div>\n" +
+    "  </div>\n" +
+    "</footer>\n"
   );
 
   $templateCache.put("anon/navbar.html",
@@ -266,69 +281,52 @@ angular.module("app").run(["$templateCache", function($templateCache) {
 
   $templateCache.put("anon/register.html",
     "<div class=\"row\">\n" +
-    "  <div class=\"welcome\">\n" +
-    "    <h2 class=\"titre center-align\">KingPong</h2>\n" +
-    "    <p>Pariatur exercitation id excepteur occaecat et labore eu labore reprehenderit ad et esse id in proident. Esse consectetur consectetur mollit adipisicing laborum adipisicing incididunt elit do exercitation. Irure cillum cillum anim voluptate voluptate\n" +
-    "      dolore anim amet excepteur id do.</p>\n" +
+    "  <div class=\"welcomeRegister background z-depth-3\">\n" +
+    "    <h2 class=\"titre center-align \">KingPong</h2>\n" +
+    "    <p>Pariatur exercitation id excepteur occaecat et labore eu labore reprehenderit ad et esse id in proident. Esse consectetur consectetur mollit adipisicing laborum adipisicing incididunt elit do exercitation.</p>\n" +
     "  </div>\n" +
-    "\n" +
     "</div>\n" +
-    "\n" +
     "<div class=\"row\">\n" +
-    "  <form class=\"col s12 l6 offset-l4 register\" name=\"loginForm\" novalidate ng-submit=\"register()\">\n" +
+    "  <form class=\"col s12 offset-l4 l4 register\" name=\"loginForm\" novalidate ng-submit=\"register()\">\n" +
     "    <div ng-repeat=\"error in errors\">{{error.error}}</div>\n" +
     "    <div class=\"row\">\n" +
-    "      <div class=\"input-field col s11 l6  \">\n" +
+    "      <div class=\"input-field col s11 offset-l1 l10  \">\n" +
     "        <i class=\"material-icons prefix\">account_circle</i>\n" +
     "        <input id=\"pseudo\" type=\"text\" class=\"form-control\" ng-model=\"user.pseudo\" required>\n" +
     "        <label for=\"pseudo\">Pseudo</label>\n" +
     "      </div>\n" +
     "    </div>\n" +
     "    <div class=\"row\">\n" +
-    "      <div class=\"input-field col s11 l6 \">\n" +
+    "      <div class=\"input-field col s11 offset-l1 l10  \">\n" +
     "        <i class=\"material-icons prefix\">mail</i>\n" +
     "        <input id=\"email\" type=\"email\" class=\"form-control\" ng-model=\"user.email\" required>\n" +
     "        <label for=\"email\">Email</label>\n" +
     "      </div>\n" +
     "    </div>\n" +
     "    <div class=\"row\">\n" +
-    "      <div class=\"input-field col s11 l6 \">\n" +
+    "      <div class=\"input-field col s11 offset-l1 l10  \">\n" +
     "        <i class=\"material-icons prefix\">vpn_key</i>\n" +
     "        <input id=\"password\" type=\"password\" class=\"form-control\" ng-model=\"user.password\" required>\n" +
     "        <label for=\"password\">Password</label>\n" +
     "      </div>\n" +
     "    </div>\n" +
     "    <div class=\"row\">\n" +
-    "      <div class=\"col s12 l6\">\n" +
+    "      <div class=\"col col s12 offset-l1 l10 \">\n" +
     "        <a id=\"submit\" ng-click=\"register()\" type=\"submit\" class=\"waves-effect waves-teal btn \">Register</a>\n" +
     "      </div>\n" +
     "    </div>\n" +
     "  </form>\n" +
     "</div>\n" +
-    "\n" +
-    "\n" +
-    "\n" +
-    "\n" +
-    "<!-- <div class=\"row\">\n" +
-    "    <div class=\"col-xs-6 col-xs-offset-3\">\n" +
-    "        <form class=\"form\" name=\"loginForm\" novalidate ng-submit=\"register()\">\n" +
-    "            <div ng-repeat=\"error in errors\">{{error.error}}</div>\n" +
-    "            <div class=\"input-group\">\n" +
-    "                <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-user\"></i></span>\n" +
-    "                <input id=\"email\" type=\"email\" class=\"form-control\" ng-model=\"user.pseudo\" required placeholder=\"Pseudo\">\n" +
-    "            </div>\n" +
-    "            <div class=\"input-group\">\n" +
-    "                <span class=\"input-group-addon\"><i class=\"material-icons\">email</i>\n" +
-    "                <input id=\"email\" type=\"email\" class=\"form-control\" ng-model=\"user.email\" required placeholder=\"Email Address\">\n" +
-    "            </div>\n" +
-    "            <div class=\"input-group\">\n" +
-    "                <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-lock\"></i></span>\n" +
-    "                <input id=\"password\" type=\"password\" class=\"form-control\" ng-model=\"user.password\" required placeholder=\"Password\">\n" +
-    "            </div>\n" +
-    "            <button type=\"submit\" class=\"btn btn-primary btn-block\">Register</button>\n" +
-    "        </form>\n" +
+    "<footer>\n" +
+    "  <div class=\"row\">\n" +
+    "    <div class=\"col s12 offset-l4 l4 center-align\">\n" +
+    "      <ul>\n" +
+    "        <li>Vous avez un compte ?</li>\n" +
+    "        <li><a class=\"center-align\" ui-sref='anon.login'>Connectez-vous</a></li>\n" +
+    "      </ul>\n" +
     "    </div>\n" +
-    "</div> -->\n"
+    "  </div>\n" +
+    "</footer>\n"
   );
 
   $templateCache.put("user/activityDescription.html",
