52a33b4948f91b3d36c8309b70f1d21b3c997397
Philippe <philippe.boudet42@gmail.com>
2017-05-11 12:25:20 +0200

    reset

--- generated/angular/template-cache.js
+++ generated/angular/template-cache.js
@@ -2,41 +2,6 @@ angular.module("app").run(["$templateCache", function($templateCache) {
 
   $templateCache.put("anon/community.html",
     "<nav>\n" +
-<<<<<<< HEAD
-    "    <div class=\"nav-wrapper\">\n" +
-    "      <form>\n" +
-    "        <div class=\"input-field center-align\">\n" +
-    "          <input id=\"search\" type=\"search\" required>\n" +
-    "          <label class=\"label-icon\" for=\"search\"><i class=\"material-icons\">search</i></label>\n" +
-    "          <i class=\"material-icons\">close</i>\n" +
-    "        </div>\n" +
-    "      </form>\n" +
-    "    </div>\n" +
-    "  </nav>\n"
-  );
-
-  $templateCache.put("anon/home.html",
-    "\n" +
-    "  \n"
-  );
-
-  $templateCache.put("anon/login.html",
-    "<div class=\"row\">\n" +
-    "    <div class=\"col-xs-6 col-xs-offset-3\">\n" +
-    "        <form class=\"form\" name=\"loginForm\" novalidate ng-submit=\"login()\">\n" +
-    "            <div ng-repeat=\"error in errors\">{{error.error}}</div>\n" +
-    "            <div class=\"input-group\">\n" +
-    "                <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-user\"></i></span>\n" +
-    "                <input id=\"email\" type=\"email\" class=\"form-control\" ng-model=\"user.email\" required placeholder=\"Email Address\">\n" +
-    "            </div>\n" +
-    "            <div class=\"input-group\">\n" +
-    "                <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-lock\"></i></span>\n" +
-    "                <input id=\"password\" type=\"password\" class=\"form-control\" ng-model=\"user.password\" required placeholder=\"Password\">\n" +
-    "            </div>\n" +
-    "            <button type=\"submit\" class=\"btn btn-primary btn-block\">Login</button>\n" +
-    "        </form>\n" +
-    "    </div>\n" +
-=======
     "  <div class=\"nav-wrapper\">\n" +
     "    <form>\n" +
     "      <div class=\"input-field center-align\">\n" +
@@ -265,7 +230,6 @@ angular.module("app").run(["$templateCache", function($templateCache) {
     "      </div>\n" +
     "    </div>\n" +
     "  </form>\n" +
->>>>>>> b240a86ddbe2b1888463c33b5c54804b7798035b
     "</div>\n"
   );
 
@@ -368,7 +332,6 @@ angular.module("app").run(["$templateCache", function($templateCache) {
   );
 
   $templateCache.put("user/activityDescription.html",
-<<<<<<< HEAD
     "<nav>\n" +
     "    <div class=\"nav-wrapper\">\n" +
     "        <form>\n" +
@@ -405,43 +368,10 @@ angular.module("app").run(["$templateCache", function($templateCache) {
     "                <div class=\"input-field col s12\">\n" +
     "                    <input id=\"teamNumber\" type=\"text\" class=\"validate\">\n" +
     "                </div>\n" +
-=======
-    "<div class=\"row\">\n" +
-    "    <div class=\"col s12\">\n" +
-    "        <md-list>\n" +
-    "            <a class=\"btn-floating btn-large waves-effect waves-light blue\">+</a>\n" +
-    "            <md-subheader class=\"md-no-sticky\">\n" +
-    "              <p>{{activity}}</p>\n" +
-    "            </md-subheader><br>\n" +
-    "            <div class=\"row\">\n" +
-    "                <form class=\"col s12\">\n" +
-    "                    <label for=\"activity\">Description</label>\n" +
-    "                    <div class=\"row\">\n" +
-    "                        <div class=\"input-field col s12\">\n" +
-    "                            <input id=\"description\" type=\"text\" class=\"active\" ng-model=\"description\">\n" +
-    "                        </div>\n" +
-    "                    </div>\n" +
-    "                    <label for=\"resultRule\">Règle du resultat</label>\n" +
-    "                    <div class=\"row\">\n" +
-    "                        <div class=\"input-field col s12\">\n" +
-    "                            <input  id=\"resultRule\" type=\"text\" class=\"validate\">\n" +
-    "                        </div>\n" +
-    "                    </div>\n" +
-    "                    <label for=\"teamNumber\">Nombre d'équipe</label>\n" +
-    "                    <div class=\"row\">\n" +
-    "                        <div class=\"input-field col s12\">\n" +
-    "                            <input id=\"teamNumber\" type=\"text\" class=\"validate\">\n" +
-    "                        </div>\n" +
-    "                    </div>\n" +
-    "                    <!-- <div class=\"row\">\n" +
-    "                        <p>PARTICIPANTS</p>\n" +
-    "                    </div> -->\n" +
->>>>>>> b240a86ddbe2b1888463c33b5c54804b7798035b
     "            </div>\n" +
     "            <label for=\"playerNumber\">Nombre de joueur par équipe</label>\n" +
     "            <div class=\"row\">\n" +
     "                <div class=\"input-field col s12\">\n" +
-<<<<<<< HEAD
     "                    <input id=\"playerNumber\" type=\"text\" class=\"validate\">\n" +
     "                </div>\n" +
     "              </div>\n" +
@@ -475,23 +405,6 @@ angular.module("app").run(["$templateCache", function($templateCache) {
     "      </form>\n" +
     "    </div>\n" +
     "  </nav>\n" +
-=======
-    "                    <input  id=\"playerNumber\" type=\"text\" class=\"validate\">\n" +
-    "                </div>\n" +
-    "            </div>\n" +
-    "            <label for=\"averageLast\">Durée moyenne</label>\n" +
-    "            <div class=\"row\">\n" +
-    "                <div class=\"input-field col s12\">\n" +
-    "                    <input  id=\"averageLast\" type=\"text\" class=\"validate\">\n" +
-    "                </div>\n" +
-    "            </div>\n" +
-    "\n" +
-    "            </form>\n"
-  );
-
-  $templateCache.put("user/createDefis.html",
-    "<a class=\"btn-floating btn-large waves-effect waves-light blue\">+</a>\n" +
->>>>>>> b240a86ddbe2b1888463c33b5c54804b7798035b
     "<br><br>\n" +
     "<div class=\"row\">\n" +
     "    <form class=\"col s12\">\n" +
@@ -523,11 +436,7 @@ angular.module("app").run(["$templateCache", function($templateCache) {
     "                <input placeholder=\"Dans quelle ville se déroulera le défi\" id=\"where\" type=\"text\" class=\"validate\">\n" +
     "            </div>\n" +
     "        </div>\n" +
-<<<<<<< HEAD
-    "<button ng-click=\"add()\"class=\"btn blue\" type=\"button\"><span>   Creer le défi    </span></button>\n" +
-=======
     "<button class=\"btn blue\" type=\"button\"><span>   Creer le défi    </span></button>\n" +
->>>>>>> b240a86ddbe2b1888463c33b5c54804b7798035b
     "</div>\n" +
     "\n" +
     "</form>\n"
@@ -538,8 +447,7 @@ angular.module("app").run(["$templateCache", function($templateCache) {
   );
 
   $templateCache.put("user/home.html",
-<<<<<<< HEAD
-    ""
+    "\n"
   );
 
   $templateCache.put("user/navbar.html",
@@ -566,55 +474,12 @@ angular.module("app").run(["$templateCache", function($templateCache) {
     "    <div class=\"col s12\">\n" +
     "\n" +
     "            <md-list>\n" +
-=======
-    "\n"
-  );
-
-  $templateCache.put("user/navbar.html",
-    "<nav class=\"navbar navbar-default\" role=\"navigation\" ng-controller=\"NavbarController\">\n" +
-    "    <div class=\"container-fluid\">\n" +
-    "        <div class=\"navbar-header\">\n" +
-    "            <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#navbar\">\n" +
-    "        <span class=\"sr-only\">Toggle navigation</span>\n" +
-    "        <span class=\"icon-bar\"></span>\n" +
-    "        <span class=\"icon-bar\"></span>\n" +
-    "        <span class=\"icon-bar\"></span>\n" +
-    "      </button>\n" +
-    "            <a class=\"navbar-brand\" href=\"#\"></a>\n" +
-    "        </div>\n" +
-    "        <div class=\"collapse navbar-collapse\" id=\"navbar\">\n" +
-    "            <ul class=\"nav navbar-nav\">\n" +
-    "                <li ui-sref-active=\"active\"><a ui-sref=\"user.dashboard\" ng-show=\"auth.isAuthenticated()\">Dashboard</a></li>\n" +
-    "                <li ui-sref-active=\"active\"><a ui-sref=\"user.profile\" ng-show=\"auth.isAuthenticated()\">Profile</a></li>\n" +
-    "              \n" +
-    "            </ul>\n" +
-    "            <ul class=\"nav navbar-nav navbar-right\">\n" +
-    "                <li>\n" +
-    "                    <li ui-sref-active=\"active\"><a ui-sref=\"anon.home\">Website</a></li>\n" +
-    "                    <li><a ng-click=\"logout()\" ng-show=\"auth.isAuthenticated()\" href='#'>Logout</a></li>\n" +
-    "                </li>\n" +
-    "            </ul>\n" +
-    "        </div>\n" +
-    "    </div>\n" +
-    "</nav>\n"
-  );
-
-  $templateCache.put("user/newDefiActivity.html",
-    "<div class=\"row\">\n" +
-    "    <div class=\"col s12\">\n" +
-    "        <md-list>\n" +
-    "            <md-subheader class=\"md-no-sticky\"><b>Activités</b></md-subheader><br>\n" +
->>>>>>> b240a86ddbe2b1888463c33b5c54804b7798035b
     "            <ul class=\"collection\">\n" +
     "                <li class=\"collection-item\" ng-repeat=\"activ in activitiesList\">\n" +
     "                    <div class=\"md-list-item-text compact\" ng-click=\"addActivity($index)\">{{activ.activity}}\n" +
     "                </li>\n" +
-<<<<<<< HEAD
     "            </ul>\n" +
     "          </md-list>\n"
-=======
-    "            </ul>\n"
->>>>>>> b240a86ddbe2b1888463c33b5c54804b7798035b
   );
 
   $templateCache.put("user/profile.html",
