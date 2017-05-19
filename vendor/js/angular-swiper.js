7c64c766906ff17723715e22dbf02707d7d5bd7a
Djodjonx <djo.moutier@gmail.com>
2017-05-19 11:12:16 +0200

    Finished slide

--- vendor/js/angular-swiper.js
+++ vendor/js/angular-swiper.js
@@ -1,11 +1,10 @@
 ! function(e, i, r) {
   "use strict";
-
   function t() {
     for (var e = [], i = "0123456789abcdef", r = 0; 36 > r; r++) e[r] = i.substr(Math.floor(16 * Math.random()), 1);
     e[14] = "4", e[19] = i.substr(3 & e[19] | 8, 1), e[8] = e[13] = e[18] = e[23] = "-";
     var t = e.join("");
-    return t
+    return t;
   }
 
   function a() {
@@ -32,10 +31,12 @@
         slideCls: "@",
         direction: "@",
         swiper: "=",
-        overrideParameters: "="
+        overrideParameters: "=",
+        slidesOffsetAfter: "="
       },
       controller: ["$scope", "$element", "$timeout", function(e, r, a) {
         var n = t();
+        console.log(e);
         e.swiper_uuid = n;
         var s = {
           slidesPerView: e.slidesPerView || 1,
@@ -44,7 +45,9 @@
           direction: e.direction || "horizontal",
           loop: e.loop || !1,
           initialSlide: e.initialSlide || 0,
-          showNavButtons: !1
+          showNavButtons: e.showNavButtons || !1,
+          swiperId: e.swiper_uuid,
+          index: e.$id
         };
         i.isUndefined(e.autoplay) || "number" != typeof e.autoplay || (s = i.extend({}, s, {
           autoplay: e.autoplay
@@ -67,8 +70,8 @@
           o = r[0];
         i.element(o.querySelector(".swiper-pagination")).attr("id", a), i.element(o.querySelector(".swiper-button-next")).attr("id", s), i.element(o.querySelector(".swiper-button-prev")).attr("id", n), i.element(r[0].querySelector(".swiper-scrollbar")).attr("id", l)
       },
-      template: '<div class="swiper-container {{containerCls}}"><div class="parallax-bg" data-swiper-parallax="{{parallaxTransition}}" ng-show="parallax"></div><div class="swiper-wrapper {{wrapperCls}}" ng-transclude></div><div class="swiper-pagination {{paginationCls}}"></div><div class="swiper-button-next" ng-show="showNavButtons"></div><div class="swiper-button-prev" ng-show="showNavButtons"></div><div class="swiper-scrollbar" ng-show="showScrollBar"></div></div>'
-    }
+      template: '<div class="swiper-container {{containerCls}}"><div class="parallax-bg" data-swiper-parallax="{{parallaxTransition}}" ng-show="parallax"></div><div class="swiper-wrapper {{wrapperCls}}" ng-transclude></div><div class="swiper-pagination {{paginationCls}}" ></div><div class="swiper-button-next hide-on-med-and-down" ng-show="showNavButtons"></div><div class="swiper-button-prev hide-on-med-and-down" ng-show="showNavButtons"></div><div class="swiper-scrollbar" ng-show="showScrollBar"></div></div>'
+    };
   }
 
   function n() {
@@ -81,7 +84,8 @@
       },
       template: '<div class="swiper-slide {{sliderCls}}" ng-transclude></div>',
       replace: !0
-    }
+    };
   }
-  i.module("ksSwiper", []).directive("ksSwiperContainer", a).directive("ksSwiperSlide", n)
+
+  i.module("ksSwiper", []).directive("ksSwiperContainer", a).directive("ksSwiperSlide", n);
 }(window, angular, void 0);
