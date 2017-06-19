dce996e2d9c0ed0760dbe708dc51cc2879f04efc
Djodjonx <djo.moutier@gmail.com>
2017-06-19 21:44:48 +0200

    re-arrange MainController and NavbarHomeController

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,83 +1,17 @@
 angular.module('app').controller('MainController', function($scope, Auth, CurrentUser, $log, $state, $window, LocalService, SharingDataService) {
 
+
   // variables
   var userId = CurrentUser.user()._id;
   var currentCommunity = CurrentUser.user().community[CurrentUser.user().community.length - 1];
-  var today = new Date();
-  var datas = {};
-  $scope.user = CurrentUser.user();
   $scope.community = {};
   $scope.invitations = {};
   $scope.arbitrages = {};
   $scope.playerChallenges = {};
   $scope.communityDefies = {};
 
-  //button
-  var axis = $window.pageYOffset;
-  $scope.showButton = true;
 
-  angular.element($window).bind("scroll", function() {
-    if (axis < $window.pageYOffset) {
-      $scope.showButton = false;
-      axis = $window.pageYOffset;
-    } else {
-      $scope.showButton = true;
-      axis = $window.pageYOffset;
-    }
-    $scope.$apply();
-  });
-
-  $scope.toCreate = function() {
-    var community = JSON.parse(LocalService.get('community'));
-    $state.go('user.createDefis', {
-      community: community._id
-    });
-  };
-
-  // //functions
-  //   function refactoringInvitations(array) {
-  //     array.map(function(element) {
-  //       element.invitation.challenge.nbPlayer = [];
-  //       element.invitation.challenge.teams.map(function(team) {
-  //         team.players.map(function(player) {
-  //           element.invitation.challenge.nbPlayer.push(player.avatar);
-  //         });
-  //       });
-  //       return element;
-  //     });
-  //     return array;
-  //   }
-  //
-  //   function refactoring(array, callback) {
-  //     array.map(function(element) {
-  //       element.challenge.nbPlayer = [];
-  //       element.challenge.teams.map(function(team) {
-  //         team.players.map(function(player) {
-  //           element.challenge.nbPlayer.push(player.avatar);
-  //         });
-  //       });
-  //       return element;
-  //     });
-  //     callback(array);
-  //   }
-  //
-  //   function filterDate(items, callback) {
-  //     var finish = [];
-  //     var notFinish = [];
-  //     items.map(function(element) {
-  //       var date = element.diff;
-  //       if (/^dans/.test(date)) {
-  //         notFinish.push(element);
-  //       } else {
-  //         finish.push(element);
-  //       }
-  //     });
-  //     callback({
-  //       finish: finish,
-  //       notFinish: notFinish
-  //     });
-  //   }
-
+//SharingDataService for adding value of NavbarHomeController
   $scope.$watch(function () { return SharingDataService.getInvitations();}, function (newValue, oldValue) {
    $scope.invitations = newValue;
  });
@@ -90,68 +24,9 @@ angular.module('app').controller('MainController', function($scope, Auth, Curren
   $scope.$watch(function () { return SharingDataService.getCommunity();}, function (newValue, oldValue) {
    $scope.communityDefies = newValue;
  });
-      
 
 
-
-
-
-  // // sidenav
-  // function buildToggler(navID) {
-  //   return function() {
-  //     $mdSidenav(navID).toggle().then(function() {
-  //       $log.debug("toggle " + navID + " is done");
-  //     });
-  //   };
-  // }
-  //
-  // $scope.onSwipeLeft = buildToggler('right');
-  //
-  // $scope.onSwipeRight = function(ev) {
-  //   $mdSidenav('right').close().then(function() {
-  //     $log.debug("close RIGHT is done");
-  //
-  //   });
-  // };
-  //
-  // $scope.toggleRight = buildToggler('right');
-  // $scope.isOpenRight = function() {
-  //   return $mdSidenav('right').isOpen();
-  // };
-  //
-  // $scope.logout = function() {
-  //   Auth.logout();
-  //   $state.go('anon.login');
-  // };
-  //
-  // // select
-  // $scope.communitys = [];
-  // UserService.getOne(userId).then(function(res) {
-  //   $scope.communitys = res.data.community;
-  //   $scope.community = $scope.communitys[$scope.communitys.length - 1];
-  //   LocalService.set('community', JSON.stringify($scope.community));
-  // });
-  //
-  // $scope.selected = function(community) {
-  //   currentCommunity = community;
-  //   LocalService.set('community', JSON.stringify(community));
-  //   launchServices(userId, community._id);
-  //
-  // };
-
-
-  // sub navbar
-  // $(document).ready(function() {
-  //   $('ul.tabs').tabs();
-  // });
-  // if ($state.current.name === 'user.home') {
-  //   $scope.selectedIndex = 0;
-  // } else {
-  //   $scope.selectedIndex = 1;
-  // }
-
-
-  // slider options
+  // slider options breakpoints
   $scope.slideOption = {
     'slidesPerView': 5,
     'spaceBetween': 20,
@@ -202,39 +77,27 @@ angular.module('app').controller('MainController', function($scope, Auth, Curren
       id: id
     });
   };
-  //Service
-  //   function launchServices(userId, currentCommunity){
-  //   InvitationService.getByUser({
-  //     player: userId,
-  //     community: currentCommunity
-  //   }).then(function(res) {
-  //     console.log(res.data);
-  //     $scope.invitations = res.data;
-  //     refactoringInvitations($scope.invitations);
-  //
-  //   });
-  //   ChallengeService.getByUser({
-  //     player: userId,
-  //     community: currentCommunity
-  //   }).then(function(res) {
-  //     refactoring(res.data, function(newData) {
-  //       filterDate(newData, function(result) {
-  //         $scope.arbitrages = result.finish;
-  //         $scope.playerChallenges = result.notFinish;
-  //       });
-  //
-  //     });
-  //   });
-  //
-  //   ChallengeService.getByCommunity(currentCommunity).then(function(res) {
-  //     refactoring(res.data, function(newData) {
-  //       filterDate(newData, function(result) {
-  //         $scope.communityDefies = result.notFinish;
-  //       });
-  //     });
-  //   });
-  //
-  // }
-  //   // TODO: supprimer les defy passé
-  // launchServices(userId, currentCommunity,null);
+
+  //newDefy buton with scroll effect
+  var axis = $window.pageYOffset;
+  $scope.showButton = true;
+
+  angular.element($window).bind("scroll", function() {
+    if (axis < $window.pageYOffset) {
+      $scope.showButton = false;
+      axis = $window.pageYOffset;
+    } else {
+      $scope.showButton = true;
+      axis = $window.pageYOffset;
+    }
+    $scope.$apply();
+  });
+
+  $scope.toCreate = function() {
+    var community = JSON.parse(LocalService.get('community'));
+    $state.go('user.createDefis', {
+      community: community._id
+    });
+  };
+
 });
--- app/js/controllers/navbarHomeController.js
+++ app/js/controllers/navbarHomeController.js
@@ -1,5 +1,7 @@
 angular.module('app')
-  .controller('NavbarHomeController', function($scope, Auth, CurrentUser, $timeout, $mdSidenav, $state, $rootScope, UserService, $log, CommunityService, $window, LocalService, InvitationService, ChallengeService,SharingDataService) {
+  .controller('NavbarHomeController', function($scope, $mdSidenav, $state, $rootScope, $log, CurrentUser, UserService, CommunityService, LocalService, InvitationService, ChallengeService, SharingDataService) {
+
+    //function for send user to community page when community is none
     $rootScope.$on('$viewContentLoaded',
       function(event) {
         if (CurrentUser.user().community.length === 0 && $state.current.name !== 'user.community') {
@@ -7,126 +9,72 @@ angular.module('app')
         }
       });
 
-      // variables
-      var userId = CurrentUser.user()._id;
-      var currentCommunity = CurrentUser.user().community[CurrentUser.user().community.length - 1];
-      var today = new Date();
-      $scope.user = CurrentUser.user();
-      $scope.community = {};
-      // $scope.invitations = {};
-      // $scope.arbitrages = {};
-      // $scope.playerChallenges = {};
-      // $scope.communityDefies = {};
 
-      //functions
-        function refactoringInvitations(array) {
-          array.map(function(element) {
-            element.invitation.challenge.nbPlayer = [];
-            element.invitation.challenge.teams.map(function(team) {
-              team.players.map(function(player) {
-                element.invitation.challenge.nbPlayer.push(player.avatar);
-              });
-            });
-            return element;
-          });
-          return array;
-        }
+    // variables
+    var userId = CurrentUser.user()._id;
+    var currentCommunity = CurrentUser.user().community[CurrentUser.user().community.length - 1];
+    var today = new Date();
+    $scope.user = CurrentUser.user();
+    $scope.community = {};
 
-        function refactoring(array, callback) {
-          array.map(function(element) {
-            element.challenge.nbPlayer = [];
-            element.challenge.teams.map(function(team) {
-              team.players.map(function(player) {
-                element.challenge.nbPlayer.push(player.avatar);
-              });
-            });
-            return element;
-          });
-          callback(array);
-        }
 
-        function filterDate(items, callback) {
-          var finish = [];
-          var notFinish = [];
-          items.map(function(element) {
-            var date = element.diff;
-            if (/^dans/.test(date)) {
-              notFinish.push(element);
-            } else {
-              finish.push(element);
-            }
+    //functions
+    function refactoringInvitations(array) {
+      array.map(function(element) {
+        element.invitation.challenge.nbPlayer = [];
+        element.invitation.challenge.teams.map(function(team) {
+          team.players.map(function(player) {
+            element.invitation.challenge.nbPlayer.push(player.avatar);
           });
-          callback({
-            finish: finish,
-            notFinish: notFinish
-          });
-        }
-
-        $(document).ready(function() {
-          $('ul.tabs').tabs();
         });
-        if ($state.current.name === 'main.home') {
-          $scope.selectedIndex = 0;
-        } else {
-          $scope.selectedIndex = 1;
-        }
-
-
-      // sidenav
-      function buildToggler(navID) {
-        return function() {
-          $mdSidenav(navID).toggle().then(function() {
-            $log.debug("toggle " + navID + " is done");
-          });
-        };
-      }
-
-      $scope.onSwipeLeft = buildToggler('right');
-
-      $scope.onSwipeRight = function(ev) {
-        $mdSidenav('right').close().then(function() {
-          $log.debug("close RIGHT is done");
-
-        });
-      };
-
-      $scope.toggleRight = buildToggler('right');
-      $scope.isOpenRight = function() {
-        return $mdSidenav('right').isOpen();
-      };
-
-      $scope.logout = function() {
-        Auth.logout();
-        $state.go('anon.login');
-      };
-
-      // select
-      $scope.communitys = [];
-      UserService.getOne(userId).then(function(res) {
-        $scope.communitys = res.data.community;
-        $scope.community = $scope.communitys[$scope.communitys.length - 1];
-        LocalService.set('community', JSON.stringify($scope.community));
+        return element;
       });
+      return array;
+    }
 
-      $scope.selected = function(community) {
-        currentCommunity = community;
-        LocalService.set('community', JSON.stringify(community));
-        launchServices(userId, community._id);
-        $state.reload('main.home');
+    function refactoring(array, callback) {
+      array.map(function(element) {
+        element.challenge.nbPlayer = [];
+        element.challenge.teams.map(function(team) {
+          team.players.map(function(player) {
+            element.challenge.nbPlayer.push(player.avatar);
+          });
+        });
+        return element;
+      });
+      callback(array);
+    }
 
-      };
+    function filterDate(items, callback) {
+      var finish = [];
+      var notFinish = [];
+      items.map(function(element) {
+        var date = element.diff;
+        if (/^dans/.test(date)) {
+          notFinish.push(element);
+        } else {
+          finish.push(element);
+        }
+      });
+      callback({
+        finish: finish,
+        notFinish: notFinish
+      });
+    }
 
-      function launchServices(userId, currentCommunity){
-        data = [];
+    function launchServices(userId, currentCommunity) {
+
+      data = [];
+
       InvitationService.getByUser({
         player: userId,
         community: currentCommunity
       }).then(function(res) {
         $scope.invitations = res.data;
         refactoringInvitations($scope.invitations);
-        SharingDataService.sendInvitations( $scope.invitations);
-
+        SharingDataService.sendInvitations($scope.invitations);
       });
+
       ChallengeService.getByUser({
         player: userId,
         community: currentCommunity
@@ -135,11 +83,9 @@ angular.module('app')
           filterDate(newData, function(result) {
             $scope.arbitrages = result.finish;
             $scope.playerChallenges = result.notFinish;
-            SharingDataService.sendArbitrages( $scope.arbitrages);
-            SharingDataService.sendPlayerDefies( $scope.playerChallenges);
-
+            SharingDataService.sendArbitrages($scope.arbitrages);
+            SharingDataService.sendPlayerDefies($scope.playerChallenges);
           });
-
         });
       });
 
@@ -153,7 +99,65 @@ angular.module('app')
         });
       });
     }
-      // TODO: supprimer les defy passé
+
+    //tabs initialization
+    $(document).ready(function() {
+      $('ul.tabs').tabs();
+    });
+    if ($state.current.name === 'main.home') {
+      $scope.selectedIndex = 0;
+    } else {
+      $scope.selectedIndex = 1;
+    }
+
+
+    // sidenav
+    function buildToggler(navID) {
+      return function() {
+        $mdSidenav(navID).toggle().then(function() {
+          $log.debug("toggle " + navID + " is done");
+        });
+      };
+    }
+
+    $scope.onSwipeLeft = buildToggler('right');
+
+    $scope.onSwipeRight = function(ev) {
+      $mdSidenav('right').close().then(function() {
+        $log.debug("close RIGHT is done");
+
+      });
+    };
+
+    $scope.toggleRight = buildToggler('right');
+    $scope.isOpenRight = function() {
+      return $mdSidenav('right').isOpen();
+    };
+
+    $scope.logout = function() {
+      Auth.logout();
+      $state.go('anon.login');
+    };
+
+
+    // select
+    $scope.communitys = [];
+    UserService.getOne(userId).then(function(res) {
+      $scope.communitys = res.data.community;
+      $scope.community = $scope.communitys[$scope.communitys.length - 1];
+      LocalService.set('community', JSON.stringify($scope.community));
+    });
+
+    $scope.selected = function(community) {
+      currentCommunity = community;
+      LocalService.set('community', JSON.stringify(community));
+      launchServices(userId, community._id);
+      $state.reload('main.home');
+
+    };
+
+
+    //launch function at start
     launchServices(userId, currentCommunity);
 
 
