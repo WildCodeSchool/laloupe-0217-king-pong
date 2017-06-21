edf4e0873213b3a2d3841f49179651deb461b412
nailletine <nailletine.lajoie19@gmail.com>
2017-06-21 15:05:30 +0200

    création de classement ordonée

--- app/js/controllers/rankController.js
+++ app/js/controllers/rankController.js
@@ -1,4 +1,49 @@
 angular.module('app')
     .controller('RankController', function($scope, CurrentUser) {
-      $scope.user = CurrentUser.user();
+        $scope.user = CurrentUser.user();
+
+
+
+        $scope.scores = [{
+
+            pseudo: 'nail',
+            avatar: 'encour',
+            classement: 2,
+            point: 5,
+            play: 13,
+            win: 7,
+            nul: 2,
+            lost: 4
+        },
+        {
+            pseudo: 'fifi',
+            avatar: 'encour',
+            classement: 1,
+            point: 5,
+            play: 18,
+            win: 9,
+            nul: 0,
+            lost: 0
+        },
+        {
+            pseudo: 'jojo',
+            avatar: 'encour',
+            classement: 3,
+            point: 5,
+            play: 13,
+            win: 7,
+            nul: 2,
+            lost: 4
+        }];
+
+var scores = $scope.scores;
+
+
+
+  for (var i = 0; i < scores.length; i++) {
+  return scores[i].classement;
+
+}
+
+
     });
