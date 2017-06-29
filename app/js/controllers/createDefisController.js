a44455943873e21b2445b3db212166639ea2e719
Djodjonx <djo.moutier@gmail.com>
2017-06-29 22:18:19 +0200

    correction style for final

--- app/js/controllers/createDefisController.js
+++ app/js/controllers/createDefisController.js
@@ -1,181 +1,165 @@
 angular.module('app')
-    .controller('CreateDefisController', function($scope, $mdDateLocale, $filter, $state, $stateParams, ActivityService, SessionService, ChallengeService, TeamService, UserService, CurrentUser, CommunityService) {
+  .controller('CreateDefisController', function($scope, $mdDateLocale, $mdDialog, $filter, $state, $stateParams, ActivityService, SessionService, ChallengeService, TeamService, UserService, CurrentUser, CommunityService) {
+
+    if (navigator.userAgent.match(/(android|iphone|blackberry|symbian|symbianos|symbos|netfront|model-orange|javaplatform|iemobile|windows phone|samsung|htc|opera mobile|opera mobi|opera mini|presto|huawei|blazer|bolt|doris|fennec|gobrowser|iris|maemo browser|mib|cldc|minimo|semc-browser|skyfire|teashark|teleca|uzard|uzardweb|meego|nokia|bb10|playbook)/gi)) {
+      $scope.device = (navigator.userAgent.match(/(android|iphone|blackberry|symbian|symbianos|symbos|netfront|model-orange|javaplatform|iemobile|windows phone|samsung|htc|opera mobile|opera mobi|opera mini|presto|huawei|blazer|bolt|doris|fennec|gobrowser|iris|maemo browser|mib|cldc|minimo|semc-browser|skyfire|teashark|teleca|uzard|uzardweb|meego|nokia|bb10|playbook)/gi)).length;
+      $scope.device = (navigator.userAgent.match(/(android|iphone|blackberry|symbian|symbianos|symbos|netfront|model-orange|javaplatform|iemobile|windows phone|samsung|htc|opera mobile|opera mobi|opera mini|presto|huawei|blazer|bolt|doris|fennec|gobrowser|iris|maemo browser|mib|cldc|minimo|semc-browser|skyfire|teashark|teleca|uzard|uzardweb|meego|nokia|bb10|playbook)/gi)).length;
+    } else {
+      $scope.device = [];
+    }
 
 
-        $scope.myDate = new Date();
+    // variables
+    var community = $stateParams.community;
+    var players = [];
+    var maxPlayers;
+    $scope.myVarBefore = false;
+    $scope.myDate = new Date();
+    $scope.currentDate = new Date();
+    $scope.user = CurrentUser.user();
+    $scope.array = [];
+    $scope.readonly = false;
+    $scope.required = true;
+    $scope.activity = JSON.parse(SessionService.get('activity') || '[]');
+    $scope.durations = [
+      "15mn",
+      "30mn",
+      "45mn",
+      "1h00",
+      "1h15",
+      "1h30",
+      "1h45",
+      "2h00"
+    ];
+    $scope.message = {
+      hour: 'Hour is required',
+      minute: 'Minute is required',
+      meridiem: 'Meridiem is required'
+    };
 
-        if (navigator.userAgent.match(/(android|iphone|blackberry|symbian|symbianos|symbos|netfront|model-orange|javaplatform|iemobile|windows phone|samsung|htc|opera mobile|opera mobi|opera mini|presto|huawei|blazer|bolt|doris|fennec|gobrowser|iris|maemo browser|mib|cldc|minimo|semc-browser|skyfire|teashark|teleca|uzard|uzardweb|meego|nokia|bb10|playbook)/gi)) {
-            $scope.device = (navigator.userAgent.match(/(android|iphone|blackberry|symbian|symbianos|symbos|netfront|model-orange|javaplatform|iemobile|windows phone|samsung|htc|opera mobile|opera mobi|opera mini|presto|huawei|blazer|bolt|doris|fennec|gobrowser|iris|maemo browser|mib|cldc|minimo|semc-browser|skyfire|teashark|teleca|uzard|uzardweb|meego|nokia|bb10|playbook)/gi)).length;
-            $scope.device = (navigator.userAgent.match(/(android|iphone|blackberry|symbian|symbianos|symbos|netfront|model-orange|javaplatform|iemobile|windows phone|samsung|htc|opera mobile|opera mobi|opera mini|presto|huawei|blazer|bolt|doris|fennec|gobrowser|iris|maemo browser|mib|cldc|minimo|semc-browser|skyfire|teashark|teleca|uzard|uzardweb|meego|nokia|bb10|playbook)/gi)).length;
+    console.log($scope.activity);
+    if($scope.activity){
+      maxPlayers = $scope.activity.numberOfTeam * $scope.activity.numberOfplayer;
+
+    }
+
+    $scope.filterActivity = function() {
+      $state.go('user.filterActivity', {
+        community: $stateParams.community
+      });
+      $scope.activity = [];
+    };
+
+
+    $scope.sendChallenge = function() {
+      $scope.newChallenge = [];
+      var Team = [];
+      var nbrTeam = $scope.activity.numberOfTeam;
+      for (var i = 1; i <= nbrTeam; i++) {
+        Team.push([]);
+      }
+      var players = [];
+      $scope.invite.forEach(function(player) {
+        players.push(player._id);
+      });
+      var infoChallenge = {
+        community: community,
+        author: $scope.user._id,
+        pseudo: $scope.user.pseudo,
+        activity: $scope.activity._id,
+        date: $scope.myDate,
+        time: $scope.startTime,
+        duration: $scope.duration,
+        place: $scope.lieu,
+        maxPlayers: $scope.activity.numberOfplayer,
+      };
+      var totalInfo = {
+        infoChallenge: infoChallenge,
+        teams: Team,
+        invite: players
+      };
+      ChallengeService.create(totalInfo).then(function(res) {});
+      $scope.array = [];
+      sessionStorage.clear();
+      $state.go('main.home');
+    };
+
+
+    $scope.goToHome = function() {
+      $state.go('main.home');
+      sessionStorage.clear();
+    };
+
+
+    $scope.toggle = function() {
+      $scope.myVarBefore = !$scope.myVarBefore;
+    };
+
+
+    $scope.check = function(checked, userId) {
+
+      if ($scope.array.length < maxPlayers - 2) {
+        if (checked) {
+          $scope.array.push(userId);
+
+
         } else {
-            $scope.device = [];
+          $scope.array.splice($scope.array.indexOf(userId), 1);
         }
 
-        var community = $stateParams.community;
-        var players = [];
-        $scope.currentDate = new Date();
+      } else {
+        $scope.array.splice($scope.array.indexOf(userId), 1);
 
-
-        CommunityService.getOne(community).then(function(res) {
-            res.data.users.forEach(function(user) {
-                user.check = false;
-            });
-            $scope.communitys = res.data.users;
-            console.log('res community', $scope.communitys);
+        $mdDialog.show({
+          contentElement: '#modalMax',
+          scope: $scope,
+          controller: 'CreateDefisController',
+          preserveScope: true,
+          hasBackdrop: false,
+          bindToController: true,
+          clickOutsideToClose: true,
+          locals: {
+            team: $scope.team
+          }
         });
-
-        $scope.user = CurrentUser.user();
-        $scope.activity = JSON.parse(SessionService.get('activity') || '[]');
-
+      }
+    };
 
 
-        // var community = "59147355f7648274a0c270e8";
+    $scope.addInvite = function() {
+      $mdDialog.hide();
 
-        console.log('nombre de joueurs par équipe : ', $scope.activity.numberOfplayer);
-        console.log('nombre d\'équipe : ', $scope.activity.numberOfTeam);
-        $scope.durations = [
-            "15mn",
-            "30mn",
-            "45mn",
-            "1h00",
-            "1h15",
-            "1h30",
-            "1h45",
-            "2h00"
-        ];
-
-        $scope.filterActivity = function() {
-            $state.go('user.filterActivity', {
-                community: $stateParams.community
-            });
-            $scope.activity = [];
-        };
-
-        $scope.sendChallenge = function() {
-
-            $scope.newChallenge = [];
+      $scope.invite = [];
+      $scope.myVarBefore = true;
+      $scope.invite = $scope.communitys.filter(function(users) {
+        return users.isChecked;
+      });
 
 
-            var Team = [];
-            var nbrTeam = $scope.activity.numberOfTeam;
-            for (var i = 1; i <= nbrTeam; i++) {
-                Team.push([]);
-                // TeamService.create({
-                //     // challenge: res.data._id,
-                //     // invite: userP,
-                //     players: [],
-                //     maxPlayer: nbrPlayer[i]
-                // });
-
-            }
-            console.log('team', Team);
-            var players = [];
-            $scope.invite.forEach(function(player) {
-                players.push(player._id);
-
-            });
+      $scope.invite = $scope.invite.map(function(users) {
+        return users;
+      });
 
 
-            var infoChallenge = {
-                community: community,
-                author: $scope.user._id,
-                pseudo: $scope.user.pseudo,
-                activity: $scope.activity._id,
-                date: $scope.myDate,
-                time: $scope.startTime,
-                duration: $scope.duration,
-                place: $scope.lieu,
-                maxPlayers: $scope.activity.numberOfplayer,
+      $scope.invite.forEach(function(player) {
+        players.push(player._id);
+      });
+      $scope.myVarBefore = !$scope.myVarBefore;
+    };
 
-            };
-            var totalInfo = {
-                infoChallenge: infoChallenge,
-                teams: Team,
-                invite: players
-
-            };
-
-            console.log('max players : ', totalInfo);
-
-            ChallengeService.create(totalInfo);
-
-            sessionStorage.clear();
-            $state.go('main.home');
-
-        };
-        $scope.goToHome = function() {
-            $state.go('main.home');
-            sessionStorage.clear();
-        };
-        $scope.myVarBefore = false;
-        $scope.toggle = function() {
-            $scope.myVarBefore = !$scope.myVarBefore;
-        };
-
-        $scope.check = function(checked, userId) {
-            $scope.array = [];
-            if (checked) {
-                $scope.array.push(userId);
-
-            } else {
-
-                $scope.array.splice($scope.array.indexOf(userId), 1);
-            }
-            console.log(checked);
-        };
-        $scope.addInvite = function() {
-            $scope.invite = [];
-            $scope.myVarBefore = true;
-            $scope.invite = $scope.communitys.filter(function(users) {
-                $scope.array = [];
-                return users.isChecked;
-            });
-            $scope.invite = $scope.invite.map(function(users) {
-
-                return users;
-            });
-
-            $scope.invite.forEach(function(player) {
-                players.push(player._id);
-
-            });
-
-            $scope.myVarBefore = !$scope.myVarBefore;
-            console.log($scope.invite);
-        };
-
-        //service
-        CommunityService.getOne(community).then(function(res) {
-            console.log(res);
-            res.data.users.forEach(function(users) {
-                users.check = false;
-            });
-            $scope.communitys = res.data.users;
-
-        });
-
-
-        // Model bound to input fields and modal
-
-        // Optional message to display below each input field
-        $scope.message = {
-            hour: 'Hour is required',
-            minute: 'Minute is required',
-            meridiem: 'Meridiem is required'
-        };
-
-        $scope.readonly = false;
-
-        $scope.required = true;
-
-
-        // TODO: limit invitation au max player -1 en comptant le créateur du defy
-        // TODO: required sur l'ensemble du formulaire pour ne pas envoyer de champ vide
-        // TODO: ne pas mettre une date antérieur à celle en cours
-        // TODO: le date picker est à la date actuel(facultatif)
-        // TODO: supprimer l'author de la liste des inviter
-
-        $mdDateLocale.formatDate = function(date) {
-            return $filter('date')($scope.myDate, "dd-MM-yyyy");
-        };
+    //service
+    CommunityService.getOne(community).then(function(res) {
+      res.data.users.forEach(function(users) {
+        users.check = false;
+      });
+      $scope.communitys = res.data.users;
     });
+
+
+    // Optional message to display below each input field
+
+
+    $mdDateLocale.formatDate = function(date) {
+      return $filter('date')($scope.myDate, "dd-MM-yyyy");
+    };
+  });
